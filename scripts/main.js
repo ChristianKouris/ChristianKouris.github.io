6b49a91721711a216e9e03cbb097bbc46e13a956
Christian Kouris <ckouris@ucsd.edu>
2021-03-20 18:34:10 -0700

    made it so that Aurebesh header it is english so user can change themes back

--- scripts/main.js
+++ scripts/main.js
@@ -18,16 +18,16 @@ function init() {
 function themeChooser(theme) {
 	switch (theme) {
 		case 'dark':
-			applyTheme(theme, 'snow', `"Times New Roman", Times, serif`, '#404040', '#202020', 'solid black', 'snow', 'rebeccapurple', '2px solid indigo', 'indianred', 'darkred', `"Brush Script MT", cursive`, `muthur, monospace`);
+			applyTheme(theme, 'snow', `"Times New Roman", Times, serif`, '#404040', '#202020', 'solid black', 'snow', `"Times New Roman", Times, serif`,'rebeccapurple', '2px solid indigo', 'indianred', 'darkred', `"Brush Script MT", cursive`, `muthur, monospace`);
 			break;
 		case 'light':
-			applyTheme(theme, 'black', `"Times New Roman", Times, serif`, 'lavenderblush', '#202020', 'solid black', 'snow', 'rebeccapurple', '2px solid indigo', 'indianred', 'darkred', `"Brush Script MT", cursive`, `muthur, monospace`);
+			applyTheme(theme, 'black', `"Times New Roman", Times, serif`, 'lavenderblush', '#202020', 'solid black', 'snow', `"Times New Roman", Times, serif`,'rebeccapurple', '2px solid indigo', 'indianred', 'darkred', `"Brush Script MT", cursive`, `muthur, monospace`);
 			break;
 		case 'muthur':
-			applyTheme(theme, '#12b853', `muthur, monospace`, 'black', 'black', 'solid black', '#12b853', 'black', '2px solid black', 'black', 'black', `muthur, monospace`, `muthur, monospace`);
+			applyTheme(theme, '#12b853', `muthur, monospace`, 'black', 'black', 'solid black', '#12b853', `muthur, monospace`, 'black', '2px solid black', 'black', 'black', `muthur, monospace`, `muthur, monospace`);
 			break;
 		case 'aurebesh':
-			applyTheme(theme, 'gold', `aurebesh, serif`, 'black', 'gold', 'solid black', 'black', 'deepskyblue', '2px solid blue', 'indianred', 'darkred', `aurebesh, cursive`, `aurebesh, monospace`);
+			applyTheme(theme, 'gold', `aurebesh, serif`, 'black', 'gold', 'solid black', 'black', `"Times New Roman", Times, serif`, 'deepskyblue', '2px solid blue', 'indianred', 'darkred', `aurebesh, cursive`, `aurebesh, monospace`);
 			break;
 		default:
 			console.log(`Invalid theme: ${theme}`);
@@ -35,15 +35,16 @@ function themeChooser(theme) {
 }
 
 /* Master function which changes all the css vars based on the theme */
-function applyTheme(id, txCo, txFo, baCo, heBa, heBo, heCo, buCo, buBo, hoBa, hoBo, faFo, teFo) {
+function applyTheme(id, txCo, txFo, baCo, heBa, heBo, heCo, heFo, buCo, buBo, hoBa, hoBo, faFo, teFo) {
 	//update local storage, hide the button and tell user what theme is equiped
 	document.getElementById(localStorage.getItem('theme')).removeAttribute('style');
 	localStorage.setItem('theme', id);
 	document.getElementById(id).setAttribute('style', 'display: none');
 	document.getElementById('themeBtn').innerHTML = `THEME: ${document.getElementById(id).innerHTML}`;
 	//update the page with the theme
-	let cssVars = ['--textcolor', '--textfont', '--backgroundcolor', '--headerbackground', '--headerborder', '--headercolor', '--buttoncolor', '--buttonborder', '--hoverbackground', '--hoverborder', '--fancyfont', '--terminalfont'];
-	let inputs = [txCo, txFo, baCo, heBa, heBo, heCo, buCo, buBo, hoBa, hoBo, faFo, teFo]
+	let cssVars = ['--textcolor', '--textfont', '--backgroundcolor', '--headerbackground', '--headerborder', '--headercolor',
+				   '--headerfont', '--buttoncolor', '--buttonborder', '--hoverbackground', '--hoverborder', '--fancyfont', '--terminalfont'];
+	let inputs = [txCo, txFo, baCo, heBa, heBo, heCo, heFo, buCo, buBo, hoBa, hoBo, faFo, teFo]
 	let r = document.querySelector(':root');
 	let i;
 	for (i = 0; i < cssVars.length; i++) {
